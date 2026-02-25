57bd4cf8f54da0c31a41fe597b4a2a1d3d4bd7f3
T-OTibe <t-otb@Turkis-MacBook-Pro.local>
2026-02-25 05:58:57 +0000

    Added dialog popup when clicking add button

--- lib/main.dart
+++ lib/main.dart
@@ -67,11 +67,18 @@ class _MyHomePageState extends State<MyHomePage> {
       'isComplete': false,
     },
   ];
+  void _showAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(title: Text('Add a Task'));
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
@@ -86,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _showAddTaskDialog,
             icon: Icon(
               Icons.add,
               color: const Color.fromARGB(255, 15, 44, 206),
